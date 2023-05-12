e20c6c7d93cd40dd4f8d91f994ba1da017b5e8fe
DianYangFu <dianyangfu@gmail.com>
2023-05-12 16:36:06 +0800

    refactor(mm-next/utils/api): adjust function `fetchHeaderDataInDefaultPageLayout` We use `Promise.all` in function `fetchHeaderDataInDefaultPageLayout` to fetch section and topics data. If one of each request is rejected, it will throw an error and no return data. The error is wrapped by package `@twreporter/errors`

--- packages/mirror-media-next/utils/api/index.js
+++ packages/mirror-media-next/utils/api/index.js
@@ -1,102 +1,56 @@
-import axios from 'axios'
-
 import errors from '@twreporter/errors'
 import {
   URL_STATIC_COMBO_SECTIONS,
   URL_STATIC_PREMIUM_SECTIONS,
   URL_STATIC_COMBO_TOPICS,
-  API_TIMEOUT,
 } from '../../config/index.mjs'
+import axiosInstance from '../../axios/index.js'
 
 /**
  * Creates an Axios request function that sends a GET request to the specified URL with a timeout.
  * @param {string} requestUrl - The URL to send the request to.
- * @returns {() => Promise<import('axios').AxiosResponse<any>>} A function that sends an Axios GET request to the specified URL and returns a Promise of the response data.
+ 
  */
 const createAxiosRequest = (requestUrl) => {
-  return () =>
-    axios({
-      method: 'get',
-      url: requestUrl,
-      timeout: API_TIMEOUT,
-    })
+  return () => axiosInstance(requestUrl)
 }
-const errorLogger = (errorMessage, globalLogFields) => {
+
+const errorLogger = (errorMessage) => {
   const annotatingAxiosError = errors.helpers.annotateAxiosError(errorMessage)
-  console.error(
-    JSON.stringify({
-      severity: 'ERROR',
-      message: errors.helpers.printAll(
-        annotatingAxiosError,
-        {
-          withStack: true,
-          withPayload: true,
-        },
-        0,
-        0
-      ),
-      ...globalLogFields,
-    })
-  )
+  throw annotatingAxiosError
 }
 const fetchNormalSections = createAxiosRequest(URL_STATIC_COMBO_SECTIONS)
 const fetchTopics = createAxiosRequest(URL_STATIC_COMBO_TOPICS)
+
 const fetchPremiumSections = createAxiosRequest(URL_STATIC_PREMIUM_SECTIONS)
 
-const fetchHeaderDataInDefaultPageLayout = async (globalLogFields = {}) => {
+const fetchHeaderDataInDefaultPageLayout = async () => {
   let sectionsData = []
   let topicsData = []
   try {
-    const responses = await Promise.allSettled([
-      fetchNormalSections(),
-      fetchTopics(),
-    ])
-
-    responses.forEach((response) => {
-      if (response.status === 'fulfilled') {
-        console.log(
-          JSON.stringify({
-            severity: 'INFO',
-            message: `Successfully fetch data on ${response.value.request.res.responseUrl}`,
-          })
-        )
-      } else {
-        const rejectedReason = response.reason
-        errorLogger(rejectedReason)
-      }
-    })
-
-    const sectionsResponse = responses[0].status === 'fulfilled' && responses[0]
-    const topicsResponse = responses[1].status === 'fulfilled' && responses[1]
-    if (Array.isArray(sectionsResponse?.value?.data._items)) {
-      sectionsData = sectionsResponse?.value?.data?._items
+    const responses = await Promise.all([fetchNormalSections(), fetchTopics()])
+    if (responses[0]?.data?._items) {
+      sectionsData = responses[0]?.data?._items
     }
-
-    if (
-      Array.isArray(topicsResponse?.value?.data?._endpoints?.topics?._items)
-    ) {
-      topicsData = topicsResponse?.value?.data?._endpoints?.topics?._items
+    if (responses[1]?.data?._endpoints?.topics?._items) {
+      topicsData = responses[1].data._endpoints.topics._items
     }
-
     return { sectionsData, topicsData }
   } catch (err) {
-    errorLogger(err, globalLogFields)
-
-    return { sectionsData, topicsData }
+    errorLogger(err)
   }
 }
 
-const fetchHeaderDataInPremiumPageLayout = async (globalLogFields = {}) => {
+const fetchHeaderDataInPremiumPageLayout = async () => {
   let sectionsData = []
   try {
     const response = await fetchPremiumSections()
     if (response?.data?.sections) {
-      sectionsData = response?.data?.sectionsColor
+      sectionsData = response?.data?.sections
     }
     return { sectionsData }
   } catch (err) {
-    errorLogger(err, globalLogFields)
-    return { sectionsData }
+    errorLogger(err)
   }
 }
 export {
